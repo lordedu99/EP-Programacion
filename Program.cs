0a03cd22e372fbd287dec91f606b469a6d949d4f
lordedu99 <eduortiz99@hotmail.com>
2025-10-03 17:34:00 -0500

    feat: migraciones automáticas y seed inicial

--- Program.cs
+++ Program.cs
@@ -94,7 +94,9 @@ using (var scope = app.Services.CreateScope())
     foreach (var role in roles)
     {
         if (!await roleManager.RoleExistsAsync(role))
+        {
             await roleManager.CreateAsync(new IdentityRole(role));
+        }
     }
 
     // Usuario Coordinador
