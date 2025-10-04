658c5f67c4a3b75ad268ba8c0d3490f27deddd16
lordedu99 <eduortiz99@hotmail.com>
2025-10-03 22:17:06 -0500

    chore(db): add SyncWithModel migration and updated snapshot

--- Data/Migrations/20251004031358_SyncWithModel.Designer.cs
+++ Data/Migrations/20251004031358_SyncWithModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PortalAcademico.Data;
@@ -11,9 +12,11 @@ using PortalAcademico.Data;
 namespace PortalAcademico.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251004031358_SyncWithModel")]
+    partial class SyncWithModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
